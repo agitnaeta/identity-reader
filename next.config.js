4d6d0db70c1a88a76df85a36eafa2e42d6a704ad
agitnaeta <agitnaeta@gmail.com>
2024-01-04 23:10:11 +0700

    add Experimental method

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = { 
+    experimental: {
+        outputFileTracingIncludes: {
+        '/api/**/*': ['./node_modules/**/*.wasm', './node_modules/**/*.proto'],
+        },
+  }
+}
 
 module.exports = nextConfig
