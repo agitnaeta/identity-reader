f10fde189656850ffb1a48257ac0b98c4ec547b9
fiadi <alfiadiansyah35@gmail.com>
2024-09-05 21:10:43 +0700

    Allow upload or read from url

---
+++ src/app/read.tsx
@@ -0,0 +1,85 @@
+import { createWorker } from 'tesseract.js';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios';
+import OpenAI from 'openai';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { promt } from '@/app/promt';
+
+
+
+export default function read(
+    image: string,
+    res: NextApiResponse
+  ) {
+    load(res,image)
+  }
+
+
+async function load(res:NextApiResponse, urlImage: string) {
+    try{
+        const worker = await createWorker('ind');
+        const ret = await worker.recognize(urlImage);
+        await worker.terminate();
+        return cleanUpGemini(ret.data.text,res)
+    }catch(error:any){
+        return res.status(500).json(error.message)
+    }
+}
+async function cleanUpGemini(text:string,res:NextApiResponse)
+{
+    const key: string  = process.env.GEMINI_AI_KEY ?? "";
+    const genAI = new GoogleGenerativeAI(key);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" ,  generationConfig: { responseMimeType: "application/json" },});
+    const prompt = promt(text)
+    const result = await model.generateContent(prompt);
+    return res.status(200).json(JSON.parse(result.response.text()))
+}
+
+
+async function cleanUp(text:string,res:NextApiResponse) {
+    const openAI = new OpenAI({
+        apiKey:process.env.OPEN_AI_KEY
+    })
+    try{
+        const completion = await openAI.chat.completions.create({
+            messages: [
+            {
+                role: "system",
+                content: "You are a helpful assistant designed to output JSON.",
+            },
+            { role: "user", content:  promt(text)},
+            ],
+            model: "gpt-4o-mini",
+            response_format: { type: "json_object" },
+        });
+        const data:any= JSON.parse(completion.choices[0].message.content ?? '')
+        return res.status(200).json(data)
+    }
+    catch(error:any){
+        console.log(error.message)
+    }
+      
+}
+
+
+
+
+async function cleanUpLama(text:string,res:NextApiResponse) {
+    const url = process.env.AI_URL+"/api/generate"; 
+    try{
+        const request = await axios.post(url,{
+            "model": "ktpai",
+            "prompt": promt(text),
+            "format": "json",
+            "stream": false
+        })
+        
+        const data:any= JSON.parse(request.data.response)
+        console.log(data)
+        return res.status(200).json(data)
+    }
+    catch(error){
+        throw new Error('Failed loading data')
+    }
+      
+}
---
+++ src/pages/api/upload.tsx
@@ -0,0 +1,66 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { promises as fs } from "fs";
+import path from "path";
+import formidable, { File, IncomingForm } from 'formidable';
+import read from '@/app/read';
+
+
+export const config = {
+    api: {
+        bodyParser: false,
+    }
+};
+
+type ProcessedFiles = Array<[string, File]>;
+
+const upload = async (req: NextApiRequest, res: NextApiResponse) => {
+
+    let status = 200,
+        resultBody = { status: 'ok', message: 'Files were uploaded successfully' };
+    let location=""; 
+
+    /* Get files using formidable */
+    const files = await new Promise<ProcessedFiles | undefined>((resolve, reject) => {
+        const form = new IncomingForm();
+        const files: ProcessedFiles = [];
+        form.on('file', function (field, file) {
+            files.push([field, file]);
+        })
+        form.on('end', () => resolve(files));
+        form.on('error', err => reject(err));
+        form.parse(req, () => {
+            //
+        });
+    }).catch(e => {
+        console.log(e);
+        status = 500;
+        resultBody = {
+            status: 'fail', message: 'Upload error'
+        }
+    });
+
+    if (files?.length) {
+
+        /* Create directory for uploads */
+        const targetPath = path.join(process.cwd(), `/public/uploads/`);
+        try {
+            await fs.access(targetPath);
+        } catch (e) {
+            await fs.mkdir(targetPath);
+        }
+
+        /* Move uploaded files to directory */
+        for (const file of files) {
+            const tempPath = file[1].filepath;
+            const random = new Date().getTime();
+            location = targetPath + random +file[1].originalFilename;
+            await fs.rename(tempPath, location);
+            read(location,res)
+        }
+    }
+    else{
+        read(location,res)
+    }
+}
+
+export default upload;
